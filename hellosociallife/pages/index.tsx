5cd325b6d2a3051b9805a9a5e4c5e2573627a0ad
ZweeDaLegend <91790736+ZweeDaLegend@users.noreply.github.com>
2025-03-31 13:20:48 +0200

    init

---
+++ hellosociallife/pages/index.tsx
@@ -0,0 +1,20 @@
+
+
+
+export default function Home() {
+
+  const arr = [1, 2, 3, 4, 5];
+
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      {arr.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+
+      <p className="text-center">
+        HEJ
+      </p>
+    </div> 
+  );
+}
---
+++ hellosociallife/pages/login.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+export default function Home() {
+    return (
+      <p>
+        Här loggar du in
+      </p>   
+    );
+  }
+  
