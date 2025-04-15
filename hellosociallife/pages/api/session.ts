b6e60ffa944d1f1dfe41b380c7016f9998e5b771
ZweeDaLegend <91790736+ZweeDaLegend@users.noreply.github.com>
2025-04-15 20:50:26 +0200

    added sessions and utils to get user from jwt

---
+++ hellosociallife/pages/api/session.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'jsonwebtoken';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const cookie = req.headers.cookie || '';
+
+
+  const authToken = cookie
+    .split(';')
+    .find((c) => c.trim().startsWith('auth_token='));
+
+  if (!authToken){
+
+    return res.status(401).json({ isLoggedIn: false });
+} 
+  const token = authToken.split('=')[1];
+  try {
+    const user = jwt.verify(token, process.env.SECRET_KEY!);
+    res.status(200).json({ isLoggedIn: true, user });
+  } catch {
+    console.error('Error decoding token:');
+    res.status(401).json({ isLoggedIn: false });
+  }
+}
