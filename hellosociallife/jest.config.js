1f0b5070a2fe99da97223ab7238bd0f449ab7e26
Zwee42 <91790736+Zwee42@users.noreply.github.com>
2025-04-16 13:34:29 +0200

    Update jest.config.js

--- hellosociallife/jest.config.js
+++ hellosociallife/jest.config.js
@@ -1,12 +1,8 @@
-import nextJest from 'next/jest'
-
-const createJestConfig = nextJest({
-  dir: './',
-})
-
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'jsdom',
-}
-
-export default createJestConfig(customJestConfig)
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  }
+};
