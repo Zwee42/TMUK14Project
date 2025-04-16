094cd7781b9788f2f9297a643d213d1e2306dc06
ZweeDaLegend <91790736+ZweeDaLegend@users.noreply.github.com>
2025-04-16 14:21:00 +0200

    added linter

--- hellosociallife/jest-preset.js
+++
@@ -1,8 +0,0 @@
-// jest-preset.js
-
-module.exports = {
-    preset: 'next/jest',
-    testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  };
-  
--- hellosociallife/jest.config.js
+++ hellosociallife/jest.config.js
@@ -1,12 +1,15 @@
-import nextJest from 'next/jest'
-
-const createJestConfig = nextJest({
-  dir: './',
-})
-
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'jsdom',
-}
-
-export default createJestConfig(customJestConfig)
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/index.ts', // Optional: exclude entrypoints
+  ],
+  coverageReporters: ['text', 'lcov'],
+};
